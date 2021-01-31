6edbadad166c5c52db859967bec29f6e1ac4cd30
tanmay-pro <tanmay.goyal@students.iiit.ac.in>
2021-01-31 10:49:35 +0530

    Template

---
+++ TEMPLATE/template.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PI 3.14159265
+#define br cout << endl
+#define fo(i, n) for (int i = 0; i < n; i++)
+#define Fo(i, k, n) for (int i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define amazing ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#define f first
+#define s second
+#define pb push_back
+#define file_read                     \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+int MOD = 1e9 + 7;
+
+int main()
+{
+    amazing;
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solution();
+    }
+    return 0;
+}
