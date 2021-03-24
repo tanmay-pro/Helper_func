6faa259bf0a9912ab478f09d14a522e09ceae94a
tanmay-pro <tanmay.goyal@students.iiit.ac.in>
2021-03-24 15:20:34 +0530

    Commit

---
+++ TEMPLATE/template.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define ll long long
+#define PI 3.14159265
+#define br printf("\n")
+#define fo(i, n) for (int i = 0; i < n; i++)
+#define Fo(i, k, n) for (int i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define file_read                     \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+int MOD = 1e9 + 7;
+
+int main()
+{
+    ll t = 1;
+    scanf("%lld", &t);
+    while (t--)
+    {
+    }
+    return 0;
+}
